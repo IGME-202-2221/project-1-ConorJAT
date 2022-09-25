74c5532dadce26d47c2a148b78e69af5642f35b2
ConorJAT <conorjat75@gmail.com>
2022-09-25 17:08:28 -0400

    Updated project with new character

---
+++ Shmup_Project1/Assets/Scripts/DragonMove.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DragonMove : MonoBehaviour
+{
+    // Speed of dragon
+    [SerializeField]
+    float speed = 1f;
+
+    // Position of dragon
+    Vector3 dragPos = Vector3.zero;
+
+    // Direction of dragon
+    Vector3 direction = Vector3.zero;
+
+    // Velocity of dragon
+    Vector3 velocity = Vector3.zero;
+
+    float totalCamHeight;
+
+    float totalCamWidth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragPos = transform.position;
+
+        Camera cam = Camera.main;
+        totalCamHeight = cam.orthographicSize * 2f;
+        totalCamWidth = totalCamHeight * cam.aspect;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Velocity = Direction * Speed * DeltaTime
+        velocity = direction * speed * Time.deltaTime;
+
+        // Adds velocity to position
+        dragPos += velocity;
+
+        // Prevents movement outside x border
+        if (dragPos.x > totalCamWidth / 2f)
+        {
+            dragPos = new Vector3(totalCamWidth / 2f, dragPos.y, 0);
+        }
+        else if (dragPos.x < -totalCamWidth / 2f)
+        {
+            dragPos = new Vector3(-totalCamWidth / 2f, dragPos.y, 0);
+        }
+
+        // Prevents movement outside y border
+        if (dragPos.y > totalCamHeight / 2f)
+        {
+            dragPos = new Vector3(dragPos.x, totalCamHeight / 2f, 0);
+        }
+        else if (dragPos.y < -totalCamHeight / 2f)
+        {
+            dragPos = new Vector3(dragPos.x, -totalCamHeight / 2f, 0);
+        }
+
+        // "Draws" vehicle to said position
+        transform.position = dragPos;
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+
+        if (direction != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        }
+    }
+}
---
+++ Shmup_Project1/Assets/Scripts/SpriteInfo.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteInfo : MonoBehaviour
+{
+    private SpriteRenderer mySprite;
+    
+
+    // Sprite Size
+    public Vector3 Size
+    {
+        get { return mySprite.bounds.size; }
+    }
+
+
+    // AABB Collision Box Related
+    public float MinX
+    {
+        get { return mySprite.bounds.min.x; }
+    }
+
+    public float MinY
+    {
+        get { return mySprite.bounds.min.y; }
+    }
+
+    public float MaxX
+    {
+        get { return mySprite.bounds.max.x; }
+    }
+
+    public float MaxY
+    {
+        get { return mySprite.bounds.max.y; }
+    }
+
+
+    // Circle Collision Box Related
+    public Vector3 Center
+    {
+        get { return mySprite.bounds.center; }
+    }
+
+    public float Radius
+    {
+        get { return mySprite.bounds.extents.y; }
+    }
+
+
+    // Sprite Color
+    public Color Color
+    {
+        get { return mySprite.color; }
+        set { mySprite.color = value; }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySprite = GetComponent<SpriteRenderer>();
+    }
+
+
+    // Update is called once per frame
+    void Update() {}
+}
